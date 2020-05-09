5ef8e90f6eb9273df99c6b6947b9bb0c43d3bdb9
kenlsm <kenlsm@outlook.com>
2020-05-09 17:12:44 +0800

    add server state sync

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 const keyStore = {};
 
 function getResyncedTime(data) {
@@ -11,9 +14,20 @@ function getResyncedTime(data) {
 }
 app.get("/:key/", (req, res) => {
   console.log(keyStore);
-  const syncedTime = getResyncedTime(keyStore[req.params.key]);
+  const entry = keyStore[req.params.key];
+  if (!entry) {
+    res.status(404);
+    res.send("unknown key");
+    return;
+  }
+  if (entry.paused) {
+    res.json({ time: entry.ctime, paused: entry.paused });
+    return;
+  }
+
+  const syncedTime = getResyncedTime(entry);
   console.log(syncedTime);
-  res.json({ time: syncedTime });
+  res.json({ time: syncedTime, paused: entry.paused });
 });
 app.get("/:key/:duration", (req, res) => {
   keyStore[req.params.key] = {
@@ -24,6 +38,17 @@ app.get("/:key/:duration", (req, res) => {
   res.send("OK");
 });
 
+app.post("/:key/:duration", (req, res) => {
+  console.log(req.body);
+  keyStore[req.params.key] = {
+    ctime: Number(req.params.duration),
+    stime: new Date() - 0,
+    paused: req.body.paused,
+  };
+  console.log(keyStore);
+  res.send("OK");
+});
+
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Setup successful http://localhost:${port}`)
 );
